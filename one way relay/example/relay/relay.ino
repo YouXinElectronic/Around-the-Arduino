1c0d08e121c91c0cff34d8e67ab4f115cdcef765
YouXinElectronic <111720573+YouXinElectronic@users.noreply.github.com>
2022-11-09 16:38:15 +0800

    relay module
    upload files

---
+++ one way relay/example/relay/relay.ino
@@ -0,0 +1,11 @@
+int Relay = 3;
+
+void setup() {
+  pinMode(Relay, OUTPUT);     //Set Pin3 as output
+}
+void loop() {
+  digitalWrite(Relay, HIGH);   //Turn on relay
+  delay(2000);
+  digitalWrite(Relay, LOW);    //Turn off relay
+  delay(2000);
+}
